1952a5220d22c220fb728d37fbdcf971a006aeea
Verberry_V <157684571+Verberry12@users.noreply.github.com>
2025-01-03 17:04:29 +0300

    feat: 42/83

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,23 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let result = false;
+  const numberSampleActivity = Number(sampleActivity);
+
+  if (typeof sampleActivity === 'string' && !isNaN(numberSampleActivity)) {
+
+    if(numberSampleActivity === 0){
+      return false;
+    }
+
+    if(numberSampleActivity > MODERN_ACTIVITY){
+        return false;
+    }
+    result = (HALF_LIFE_PERIOD / Math.log(2)) * Math.log(MODERN_ACTIVITY / numberSampleActivity);
+  }
+
+  return result ? Math.ceil(result) : false;
 }
 
 module.exports = {
